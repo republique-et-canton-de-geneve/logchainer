ab3e02d5bc716f143fae95538a987e054b716e25
Fanichet Loïc (DSE) <loic.fanichet@etat.ge.ch>
2018-03-16 15:43:08 +0200

    ajout de tests unitaires - LOGCHAINER-1

--- logchainer/src/test/java/ch/ge/logchainer/service/HashServiceTest.java
+++ logchainer/src/test/java/ch/ge/logchainer/service/HashServiceTest.java
@@ -1,5 +1,20 @@
 package ch.ge.logchainer.service;
 
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+import ch.ge.cti.logchainer.service.HashService;
+
 public class HashServiceTest {
+
+    @Test(description = "hash method test")
+    public void testHashCode() {
+	String pathTestFile = "D:/_codesource_M501/logchainer-base/logchainer/src/test/resources/testHashCode";
+	byte[] refArray = new byte[] { 26, 40, -50, 107, -109, -40, 41, -24, -95, -37, 5, -6, 73, -56, 18, -71, -18,
+		-13, 11, 115, -80, -74, 113, 28, -80, 95, 26, 110, 10, -38, 4, -49 };
+
+	assertEquals(HashService.getLogHashCode(pathTestFile), refArray);
+    }
 
 }
--- logchainer/src/test/java/ch/ge/logchainer/service/LogWatcherServiceTest.java
+++ logchainer/src/test/java/ch/ge/logchainer/service/LogWatcherServiceTest.java
@@ -1,5 +1,97 @@
 package ch.ge.logchainer.service;
 
+import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertTrue;
+
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 public class LogWatcherServiceTest {
+    private WatchService watchService;
+    private WatchKey basePathWatchKey;
+    private String testResourcesDirPath = "D:/_codesource_M501/logchainer-base/logchainer/src/test/resources";
+
+    @BeforeTest
+    public void setUp() throws Exception {
+	watchService = FileSystems.getDefault().newWatchService();
+	basePathWatchKey = Paths.get(testResourcesDirPath).register(watchService, ENTRY_CREATE);
+    }
+
+    @Test
+    public void testEventForDirectory() throws Exception {
+	String noData = "";
+	Files.write(Paths.get(testResourcesDirPath + "/testFile1.txt"), noData.getBytes());
+	Files.write(Paths.get(testResourcesDirPath + "/testFile2.txt"), noData.getBytes());
+	Files.write(Paths.get(testResourcesDirPath + "/testFile3.txt"), noData.getBytes());
+
+	WatchKey watchKey = watchService.poll(20, TimeUnit.SECONDS);
+	assertNotNull(watchKey);
+	assertEquals(watchKey, basePathWatchKey);
+	List<WatchEvent<?>> eventList = watchKey.pollEvents();
+	assertEquals(eventList.size(), 3);
+	for (WatchEvent event : eventList) {
+	    assertTrue(event.kind() == StandardWatchEventKinds.ENTRY_CREATE);
+	    assertEquals(event.count(), 1);
+	}
+
+	Path eventPath = Paths.get(testResourcesDirPath + "/" + eventList.get(0).context().toString());
+	assertTrue(Files.isSameFile(eventPath, Paths.get(testResourcesDirPath + "/testFile1.txt")));
+
+	Path watchedPath = (Path) watchKey.watchable();
+	assertTrue(Files.isSameFile(watchedPath, Paths.get(testResourcesDirPath)));
+
+	Files.delete(Paths.get(testResourcesDirPath + "/testFile1.txt"));
+	Files.delete(Paths.get(testResourcesDirPath + "/testFile2.txt"));
+	Files.delete(Paths.get(testResourcesDirPath + "/testFile3.txt"));
+    }
+
+    @Test
+    public void testEventForDirectoryWatchKey() throws Exception {
+	String noData = "";
+	Files.write(Paths.get(testResourcesDirPath + "/testFile4.txt"), noData.getBytes());
+
+	List<WatchEvent<?>> eventList = basePathWatchKey.pollEvents();
+	while (eventList.size() == 0) {
+	    eventList = basePathWatchKey.pollEvents();
+	    Thread.sleep(10000);
+	}
+
+	assertEquals(eventList.size(), 1);
+	for (WatchEvent event : eventList) {
+	    assertTrue(event.kind() == StandardWatchEventKinds.ENTRY_CREATE);
+	}
+
+	basePathWatchKey.reset();
+
+	Files.write(Paths.get(testResourcesDirPath + "/testFile5.txt"), noData.getBytes());
+	Files.write(Paths.get(testResourcesDirPath + "/testFile6.txt"), noData.getBytes());
+	while (eventList.size() == 0) {
+	    eventList = basePathWatchKey.pollEvents();
+	    Thread.sleep(10000);
+	}
+
+	Path eventPath = Paths.get(testResourcesDirPath + "/" + eventList.get(0).context().toString());
+	assertTrue(Files.isSameFile(eventPath, Paths.get(testResourcesDirPath + "/testFile4.txt")));
+
+	Path watchedPath = (Path) basePathWatchKey.watchable();
+	assertTrue(Files.isSameFile(watchedPath, Paths.get(testResourcesDirPath)));
+
+	Files.delete(Paths.get(testResourcesDirPath + "/testFile4.txt"));
+	Files.delete(Paths.get(testResourcesDirPath + "/testFile5.txt"));
+	Files.delete(Paths.get(testResourcesDirPath + "/testFile6.txt"));
+    }
 
 }
