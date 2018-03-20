f7e729047c916496990dc5bf1d583611d71ba0eb
Fanichet Loïc (DSE) <loic.fanichet@etat.ge.ch>
2018-03-20 13:14:14 +0200

    no message

--- logchainer/src/main/java/ch/ge/cti/logchainer/constante/LogChainerConstante.java
+++ logchainer/src/main/java/ch/ge/cti/logchainer/constante/LogChainerConstante.java
@@ -2,7 +2,7 @@ package ch.ge.cti.logchainer.constante;
 
 public class LogChainerConstante {
     
-    public static final String FILENAME = "src/main/resources/configurations.properties";
+    public static final String FILENAME = "src/main/resources/conf/configurations.properties";
     
     public static final String INPUT_DIRECTORY = "inputDirectory";
     
