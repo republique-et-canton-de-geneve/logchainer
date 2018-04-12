146d4bafff66dbfc2caf12ddd653f3a0e3fb3c4b
Fanichet Loïc (DSE) <loic.fanichet@etat.ge.ch>
2018-04-12 09:10:14 +0200

    fin comms

--- logchainer/src/main/java/ch/ge/cti/logchainer/service/properties/UtilsComponents.java
+++ logchainer/src/main/java/ch/ge/cti/logchainer/service/properties/UtilsComponents.java
@@ -3,10 +3,30 @@ package ch.ge.cti.logchainer.service.properties;
 import ch.ge.cti.logchainer.beans.Client;
 
 public interface UtilsComponents {
-    
+    /**
+     * Get the configured sorter String or the default one if none is defined by
+     * the user. Default is "numerical".
+     * 
+     * @param client
+     * @return the sorter
+     */
     String getSorter(Client client);
-    
+
+    /**
+     * Get the configured separator String or the default one if none is defined by
+     * the user. Default is "_".
+     * 
+     * @param client
+     * @return the separator
+     */
     String getSeparator(Client client);
-    
+
+    /**
+     * Get the configured encoding type String or the default one if none is defined by
+     * the user. Default is "UTF-8".
+     * 
+     * @param client
+     * @return the encoding type
+     */
     String getEncodingType(Client client);
 }
