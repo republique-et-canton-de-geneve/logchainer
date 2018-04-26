21a5fdb4f94c767a0a72e9035208749305409d34
Fanichet Loïc (DSE) <loic.fanichet@etat.ge.ch>
2018-04-26 12:43:15 +0200

    no message

--- logchainer/src/test/java/ch/ge/cti/logchainer/service/logwatcher/LogWatcherServiceTest.java
+++ logchainer/src/test/java/ch/ge/cti/logchainer/service/logwatcher/LogWatcherServiceTest.java
@@ -125,7 +125,7 @@ public class LogWatcherServiceTest {
 	}
 	int actualTime = LocalDateTime.now().getHour() * CONVERT_HOUR_TO_SECONDS
 		+ LocalDateTime.now().getMinute() * CONVERT_MINUTE_TO_SECONDS + LocalDateTime.now().getSecond();
-//	assertTrue(actualTime - testFile.getArrivingTime() > DELAY_TRANSFER_FILE);
+	assertTrue(actualTime - testFile.getArrivingTime() > DELAY_TRANSFER_FILE);
     }
 
     @Test(description = "testing the removal of a file after it's process")
