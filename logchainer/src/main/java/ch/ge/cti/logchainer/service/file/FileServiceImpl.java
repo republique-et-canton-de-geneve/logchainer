249bf638928af31a8ed52c1abb8140999286db88
Courtois Julien (DSE) <julien.courtois@etat.ge.ch>
2019-11-22 15:52:00 +0100

    LOGCHAINER-14

--- logchainer/src/main/java/ch/ge/cti/logchainer/service/file/FileServiceImpl.java
+++ logchainer/src/main/java/ch/ge/cti/logchainer/service/file/FileServiceImpl.java
@@ -36,6 +36,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import ch.ge.cti.logchainer.beans.Client;
 import ch.ge.cti.logchainer.beans.WatchedFile;
